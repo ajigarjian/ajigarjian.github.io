323ae9783248cc38f4c1996d947ee132a94c4660
Ari Jigarjian <ajigarjian@gmail.com>
2021-03-26 18:30:39 -0400

    Update source.js

--- source.js
+++ source.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $.getJSON("data1.json", function(data) {
+  $.getJSON("https://github.com/ajigarjian/ajigarjian.github.io/blob/main/data1.json", function(data) {
     console.log('Success');
   })
 })
